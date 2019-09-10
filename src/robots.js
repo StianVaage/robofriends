e000f90f2fc9f68ba8217ec1d871bb6a05ea73d9
Stian Vaage <stianvaage@gmail.com>
2019-09-11 01:17:09 +0200

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Stian Vaage",
+    username: "gaffel",
+    email: "stianvaage@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Ine Malen Stock",
+    username: "finemalen",
+    email: "inemalens@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Lotte",
+    username: "Lottelini",
+    email: "lotte@verdensbestehund.no"
+  },
+  {
+    id: 4,
+    name: "Missy",
+    username: "Missy",
+    email: "katter@harikkeemail.no"
+  }
+];
